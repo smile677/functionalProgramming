6a322acbde72940a82e9c510c9e2bde47d2ca884
smile67 <1639316332@qq.com>
2024-08-04 11:54:47 +0800

    函数接口-JDK

--- src/main/java/com/smile67/day2/CategoryTest.java
+++ src/main/java/com/smile67/day2/CategoryTest.java
@@ -42,31 +42,41 @@ public class CategoryTest {
 
     public static void main(String[] args) {
 //        Type1 obj1 = (int a) -> (a & 1) == 0;
-        Type1 obj1 = a -> (a & 1) == 0;
+//        Type1 obj1 = a -> (a & 1) == 0;
 
 //        Type1 obj2 = (int a) -> BigInteger.valueOf(a).isProbablePrime(100);
-        Type1 obj2 = a -> BigInteger.valueOf(a).isProbablePrime(100);
+//        Type1 obj2 = a -> BigInteger.valueOf(a).isProbablePrime(100);
 
 //        Type2 obj3 = (int a, int b, int c) -> a + b + c;
-        Type2 obj3 = (a, b, c) -> a + b + c;
+//        Type2 obj3 = (a, b, c) -> a + b + c;
 
 //        Type3 obj4 = (int a, int b) -> a - b;
-        Type3 obj4 = (a, b) -> a - b;
+//        Type3 obj4 = (a, b) -> a - b;
 
 //        Type3 obj5 = (int a, int b) -> a * b;
-        Type3 obj5 = (a, b) -> a * b;
+//        Type3 obj5 = (a, b) -> a * b;
 
-        Type4 obj6 = () -> new Student();
+//        Type4 obj6 = () -> new Student();
 //        Type6<Student> obj6 = () -> new Student();
 
-        Type5 obj7 = () -> new ArrayList<Student>();
+//        Type5 obj7 = () -> new ArrayList<Student>();
 //        Type6<List<Student>> obj7 = () -> new ArrayList<Student>();
 
 //        Type7<String, Student> obj8 = (Student s) -> s.getName();
-        Type7<String, Student> obj8 = s -> s.getName(); // 优化后
+//        Type7<String, Student> obj8 = s -> s.getName(); // 优化后
 
 //        Type7<Integer, Student> obj9 = (Student s) -> s.getAge();
-        Type7<Integer, Student> obj9 = s -> s.getAge();
+//        Type7<Integer, Student> obj9 = s -> s.getAge();
+
+        IntPredicate obj1 = a -> (a & 1) == 0;
+        IntPredicate obj2 = a -> BigInteger.valueOf(a).isProbablePrime(100);
+        IntTernaryOperator obj3 = (a, b, c) -> a + b + c;
+        IntBinaryOperator obj4 = (a, b) -> a - b;
+        IntBinaryOperator obj5 = (a, b) -> a * b;
+        Supplier<Student> obj6 = () -> new Student();
+        Supplier<List<Student>> obj7 = () -> new ArrayList<Student>();
+        Function<Student, String> obj8 = s -> s.getName();
+        Function<Student, Integer> obj9 = s -> s.getAge();
     }
 
 
@@ -76,7 +86,7 @@ public class CategoryTest {
     }
 
     @FunctionalInterface
-    interface Type2 {
+    interface IntTernaryOperator {
         int op(int a, int b, int c);
     }
 
